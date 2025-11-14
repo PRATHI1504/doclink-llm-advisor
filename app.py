70ec477dea0b974b894910a9049377d85d6abe63
PRATHI1504 <168351588+PRATHI1504@users.noreply.github.com>
2025-11-14 13:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 from dotenv import load_dotenv
 from groq import Groq
 from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 import streamlit as st
